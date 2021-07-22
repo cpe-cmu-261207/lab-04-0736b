143d245335aca832b863c31d5660ba2c15c1f313
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-07-22 21:50:47 +0700

    70 percent

--- script.js
+++ script.js
@@ -1,99 +1,162 @@
 const button = document.querySelector('#enter');
 const input = document.querySelector('#userInput');
-const divList = document.querySelector('#divList');
-const ul = document.createElement('ul');
-ul.className = "text-center mx-auto";
+const divDone = document.querySelector('#done');
+const divUndone = document.querySelector('#todo');
+const todoForm = document.querySelector('#todo-form');
+const hr = document.createElement('hr');
+
+//LocalStorage
 let todos = [];
-let dones = [];
+let finish = [];
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addTodoAfterClick(input.value);
+});
 
 
+//InputLength
 const inputLength = () => {
     return input.value.length;
 }
 
+//Render HTML
+const renderTodos = (todos) => {
+    divDone.innerHTML = '';
+    divUndone.innerHTML = '';
 
-const listInit = (task) => {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(task));
-    li.className = "flex justify-between space-x-4 px-4 py-6 group text-xl";
-    const divButton = document.createElement('div');
-    divButton.className = "m-0 px-4 opacity-0 group-hover:opacity-100 space-x-1 transition duration-300 ease-out"
-    const btnDone = document.createElement('button');
-    btnDone.innerHTML = "DONE";
-    btnDone.className ="font-sans text-sm text-black font-normal px-4 bg-green-600 hover:bg-green-700 transition duration-200 ease-in-out";
-    const btnDel = document.createElement('button');
-    btnDel.innerHTML = "DELETE";
-    btnDel.className ="font-sans deletebtn text-sm text-black font-normal px-4 bg-red-700 hover:bg-red-800 transition duration-200 ease-in-out";
-    const hr = document.createElement('hr');
-    hr.className = "font-black";
-    divButton.appendChild(btnDone);
-    divButton.appendChild(btnDel);
-    li.appendChild(divButton);
-    ul.appendChild(li);
-    ul.appendChild(hr);
-    // btnDel.addEventListener('click', () => {
-	// 	ul.removeChild(li);
-	// });
-    // btnDone.addEventListener('click', () => {
-    //     li.className = "flex justify-between space-x-4 px-4 py-6 group text-xl done";
-    // });
+    todos.forEach(item => {
+        const checked = item.done ? 'checked': null;
+
+        const div1 = document.createElement('div');
+        div1.className = "flex justify-between space-x-4 px-4 py-6 group text-xl alpaga";
+
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+        if(item.done === true){
+            li.classList.add('checked');
+        }
+        
+        li.innerHTML = `${item.name}`;
+
+        const divButton = document.createElement('div');
+        const DoneButton = document.createElement('button');
+        DoneButton.setAttribute('done-key',item.id);
+        DoneButton.className = "done-button";
+        DoneButton.innerHTML = "Done";
+        divButton.append(DoneButton);
+        const DelButton = document.createElement('button');
+        DelButton.setAttribute('delete-key',item.id);
+        DelButton.className = "delete-button";
+        DelButton.innerHTML = "Delete";
+        divButton.append(DelButton);
+        if(item.done == false){
+            div1.append(li);
+            div1.append(divButton);
+            divUndone.append(div1);
+            divUndone.append(hr);
+        }
+        if(item.done == true) {
+            li.classList.add('line-through');
+            div1.append(li);
+            divDone.append(div1);
+            divUndone.append(hr);
+        }
+    });
 }
 
 
+const addToLocalStorage = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderTodos(todos);
+}
 
-const createListElement = () => {
-
-
-
-
-    // const li = document.createElement('li');
-    // li.appendChild(document.createTextNode(input.value));
-    // li.className = "flex justify-between space-x-4 px-4 py-6 group text-xl";
-    // const divButton = document.createElement('div');
-    // divButton.className = "m-0 px-4 opacity-0 group-hover:opacity-100 space-x-1 transition duration-300 ease-out"
-    // const btnDone = document.createElement('button');
-    // btnDone.innerHTML = "DONE";
-    // btnDone.className ="font-sans text-sm text-black font-normal px-4 bg-green-600 hover:bg-green-700 transition duration-200 ease-in-out";
-    // const btnDel = document.createElement('button');
-    // btnDel.innerHTML = "DELETE";
-    // btnDel.className ="font-sans deletebtn text-sm text-black font-normal px-4 bg-red-700 hover:bg-red-800 transition duration-200 ease-in-out";
-    // const hr = document.createElement('hr');
-    // hr.className = "font-black";
-    // divButton.appendChild(btnDone);
-    // divButton.appendChild(btnDel);
-    // li.appendChild(divButton);
-    // ul.appendChild(li);
-    // ul.appendChild(hr);
-    // btnDel.addEventListener('click', () => {
-	// 	ul.removeChild(li);
-	// });
-    // btnDone.addEventListener('click', () => {
-    //     li.className = "flex justify-between space-x-4 px-4 py-6 group text-xl done";
-    // });
-
-    input.value = "";
+const getFromLocalStorage = () => {
+    const ref = localStorage.getItem('todos');
+    if(ref) {
+        todos = JSON.parse(ref);
+        renderTodos(todos);
+    }
 }
 
 
-divList.appendChild(ul);
-
-const addListAfterClick = () => {
+// Click
+const addTodoAfterClick = (item) => {
     if(inputLength() > 0){
-        createListElement();
+        const todo = {
+            id: Date.now(),
+            name: item,
+            done: false
+        };
+        todos.unshift(todo);
+        addToLocalStorage(todos);
+        input.value = '';
     }
     else if(inputLength() === 0){
         alert('Task cannot be empty!');
     }
 }
 
-const addListAfterKeypress = (event) => {
+
+
+// Press Enter
+const addTodoAfterKeypress = (event) => {
     if(inputLength() > 0 && event.keyCode === 13){
-        createListElement();
+        const todo = {
+            id: Date.now(),
+            name: item,
+            done: false
+        };
+        todos.unshift(todo);
+        addToLocalStorage(todos);
+        input.value = '';
     }
     else if(inputLength() === 0 && event.keyCode === 13){
         alert('Task cannot be empty!');
     }
 }
 
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
+
+
+// button.addEventListener("click", addListAfterClick);
+// input.addEventListener("keypress", addListAfterKeypress);
+getFromLocalStorage();
+
+
+
+
+const checkDoneAndDel = (event) => {
+    if(event.target.classList.contains('delete-button')){
+        console.log(event.target);
+        console.log(event.target.getAttribute('delete-key'));
+        deleteTodo(event.target.getAttribute('delete-key'));
+    }
+
+    if(event.target.classList.contains('done-button')){
+        console.log(event.target);
+        console.log(event.target.getAttribute('done-key'));
+        doneTodo(event.target.getAttribute('done-key'));
+    }
+}
+
+const doneTodo = (id) => {
+    todos.forEach( (item) => {
+        if(item.id == id){
+            item.done = true;
+        }
+    });
+    addToLocalStorage(todos);
+}
+
+const deleteTodo = (id) => {
+    todos = todos.filter( (item) => {
+        return item.id != id;
+    });
+
+    addToLocalStorage(todos);
+}
+
+divUndone.addEventListener('click',checkDoneAndDel);
+
+
